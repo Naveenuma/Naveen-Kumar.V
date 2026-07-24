27328ff6019889dd89de63ccb055a1d222eb4438
Naveen kumar <165548267+Naveenuma@users.noreply.github.com>
2026-07-24 15:32:15 +0530

    Add files via upload

--- naveen.py
+++ naveen.py
@@ -1,16 +1,20 @@
 n = int(input())
-count=0
-num=101
-while count<n:
-    prime=True
-    for i in range(2,int(num**0.5)+1):
-        if num%i==0:
-            prime=False
-            break
-    if prime:
-        if count>0:
-            print(",",end="")
-        print(num,end="")
-        count+=1
-    num+=1 
-    
+
+temp = n
+digits = 0
+
+while temp > 0:
+    digits += 1
+    temp //= 10
+
+temp = n
+total = 0
+
+while temp > 0:
+    digit = temp % 10
+    total += digit**digits
+    temp //= 10
+if total == n:
+    print("Amstrong Number")
+else:
+    print("Not a Amstrong Number")
